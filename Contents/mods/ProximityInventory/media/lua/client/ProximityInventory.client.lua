8d558f05bee24b4afc9b9a0dfec3e120ddaaac93
buffyuwu <42501819+buffyuwu@users.noreply.github.com>
2022-08-12 14:39:38 -0400

    patching locked containers

--- Contents/mods/ProximityInventory/media/lua/client/ProximityInventory.client.lua
+++ Contents/mods/ProximityInventory/media/lua/client/ProximityInventory.client.lua
@@ -69,11 +69,11 @@ function ISInventoryPage:addContainerButton(container, texture, name, tooltip)
 		containerButton.capacity = 0
 	end
 
-	if container:getType() ~= "local" and ProximityInventory.canBeAdded(container) then
+        if container:getType() ~= "local" and ProximityInventory.canBeAdded(container) and not container:isLockedToCharacter(self.player) then
 		local localItems = localContainer:getItems()
 		local items = container:getItems()
 		localItems:addAll(items)
-	end
+        end
 
 	if container:getType() == "floor" then
 		ISInventoryPage.canInjectButton = true
