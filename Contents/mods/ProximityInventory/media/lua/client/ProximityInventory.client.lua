6987522fdaf1d40f4cd7041298dd96621c890905
Massimo Cacciapaglia <massimo.cacciapaglia@nexusmods.com>
2022-08-13 01:01:41 +0100

    Added toggle

--- Contents/mods/ProximityInventory/media/lua/client/ProximityInventory.client.lua
+++ Contents/mods/ProximityInventory/media/lua/client/ProximityInventory.client.lua
@@ -10,6 +10,7 @@ require "defines"
 local inventoryIcon = getTexture("media/ui/ProximityInventory.png")
 
 ProximityInventory = {}
+ProximityInventory.isToggled = false
 ProximityInventory.inventoryIcon = getTexture("media/ui/ProximityInventory.png")
 ProximityInventory.bannedTypes = {
 	stove = true,
@@ -33,6 +34,11 @@ ProximityInventory.canBeAdded = function (container, playerObj)
 	return not ProximityInventory.bannedTypes[container:getType()]
 end
 
+function ISInventoryPage:toggleProximityInv()
+	ProximityInventory.isToggled = not ProximityInventory.isToggled
+	self:refreshBackpacks()
+end
+
 function ISInventoryPage.GetLocalContainer(playerNum)
 	if ISInventoryPage.localContainer == nil then
 		ISInventoryPage.localContainer = {}
@@ -47,7 +53,6 @@ end
 
 local old_ISInventoryPage_GetFloorContainer = ISInventoryPage.GetFloorContainer
 function ISInventoryPage.GetFloorContainer(playerNum)
-
 	local localContainer = ISInventoryPage.GetLocalContainer(playerNum)
 	local container = localContainer
 	container:removeItemsFromProcessItems()
@@ -56,14 +61,14 @@ function ISInventoryPage.GetFloorContainer(playerNum)
 	return old_ISInventoryPage_GetFloorContainer(playerNum)
 end
 
-ISInventoryPage.canInjectButton = true;
 
 local old_ISInventoryPage_addContainerButton = ISInventoryPage.addContainerButton
+ISInventoryPage.canInjectButton = true;
 function ISInventoryPage:addContainerButton(container, texture, name, tooltip)
-	if self.onCharacter then
+	if self.onCharacter or not ProximityInventory.isToggled then
 		return old_ISInventoryPage_addContainerButton(self, container, texture, name, tooltip)
 	end
-	
+
 	local localContainer = ISInventoryPage.GetLocalContainer(self.player)
 	if ISInventoryPage.canInjectButton then
 		ISInventoryPage.canInjectButton = false
@@ -71,6 +76,7 @@ function ISInventoryPage:addContainerButton(container, texture, name, tooltip)
 		local title = "Proximity Inventory"
 		local containerButton = self:addContainerButton(localContainer, inventoryIcon, title, title)
 		containerButton.capacity = 0
+		self:setForceSelectedContainer(containerButton.inventory)
 	end
 
 	local playerObj = getSpecificPlayer(self.player)
@@ -86,4 +92,26 @@ function ISInventoryPage:addContainerButton(container, texture, name, tooltip)
 
 	-- can't cache the result, otherwise the prox inv won't be the first item
 	return old_ISInventoryPage_addContainerButton(self, container, texture, name, tooltip)
+end
+
+local old_ISInventoryPage_createChildren = ISInventoryPage.createChildren
+function ISInventoryPage:createChildren()
+	local result = old_ISInventoryPage_createChildren(self)
+
+	if not self.onCharacter then
+		local lootButtonHeight = self:titleBarHeight()
+		local removeAllRight = self.removeAll:getRight();
+		local toggleStoveRight = self.toggleStove:getRight();
+		local rightOffset = math.max(removeAllRight, toggleStoveRight);
+
+		self.toggleProximityInv = ISButton:new(self.lootAll:getRight() + rightOffset + 16, 0, 50, lootButtonHeight, 'Toggle Proximity Inventory', self, ISInventoryPage.toggleProximityInv);
+        self.toggleProximityInv:initialise();
+        self.toggleProximityInv.borderColor.a = 0.0;
+        self.toggleProximityInv.backgroundColor.a = 0.0;
+        self.toggleProximityInv.backgroundColorMouseOver.a = 0.7;
+        self:addChild(self.toggleProximityInv);
+        self.toggleProximityInv:setVisible(true);
+	end
+
+	return result
 end
